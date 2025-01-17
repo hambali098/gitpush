7b7a85bdd5d418f6bd13a58c21f2498a4458aabb
hambali098 <hambalitubagus@gmail.com>
2025-01-17 07:14:27 +0700

    hsahdhas

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">apakah bisa? hahahaha</h1>
+      <h1 className="text-4xl font-bold">apakah bisa? apakah masih error?</h1>
     </main>
   )
 }
